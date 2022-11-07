3974d0f7c6d2c1dbfcc2e92ddb2d6d8d6508f4f2
abrarasif11 <faasif929@gmail.com>
2022-11-08 00:40:21 +0600

    connected-mongodb

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,6 +13,37 @@ app.get('/', (req, res) =>{
     res.send('Tourist Site Server is Running')
 })
 
+
+
 app.listen(port, () =>{
     console.log(`Tourist Server Running on ${port}`);
 })
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vhdpi0m.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const serviceCollection = client.db('travelHunterdb').collection('services');
+        app.get('/services', async(req, res) =>{
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: (id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        })
+    }
+        finally{
+
+        }
+    }
+    run().catch(err => console.error(err));
+        
